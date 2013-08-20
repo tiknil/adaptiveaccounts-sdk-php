3ff6a11abe1a64dfaabca31c4925a049eb54dabe
Latha <lathavairamani@gmail.com>
2013-08-20 23:56:20 +0530

    Update Configuration.php

--- samples/Configuration.php
+++ samples/Configuration.php
@@ -12,8 +12,7 @@ class Configuration
 				// These values are defaulted in SDK. If you want to override default values, uncomment it and add your value.
 				// "http.ConnectionTimeOut" => "5000",
 				// "http.Retry" => "2",
-				// "http.ReadTimeOut" => "30000",
-				// "http.MaxConnection" => "100"
+				
 		);
 		return $config;
 	}
@@ -26,7 +25,7 @@ class Configuration
 				"acct1.UserName" => "jb-us-seller_api1.paypal.com",
 				"acct1.Password" => "WX4WTU3S8MY44S7F",
 				"acct1.Signature" => "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy",
-				"acct1.AppId" => "APP-80W284485P519543T"
+				"acct1.AppId" => "APP-80W284485P519543T",
 				
 				// Sample Certificate Credential
 				// "acct1.UserName" => "certuser_biz_api1.paypal.com",
