afd3e14e33b4cb274a547be79b043c3948de60a9
japatel <japatel@paypal.com>
2014-11-11 11:29:13 -0600

    Added install.php for namespaced libraries.
    - Updated version number in user agent.
    - Fixed composer.json to use generic version

--- lib/PayPal/Service/AdaptiveAccountsService.php
+++ lib/PayPal/Service/AdaptiveAccountsService.php
@@ -31,7 +31,7 @@ class AdaptiveAccountsService extends PPBaseService {
 	protected static $SDK_NAME = "adaptiveaccounts-php-sdk";
 
 	// SDK Version
-	protected static $SDK_VERSION = "3.8.106";
+	protected static $SDK_VERSION = "3.8.107";
 
     /**
     * @param $config - Dynamic config map. This takes the higher precedence if config file is also present.
